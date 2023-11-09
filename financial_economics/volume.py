fe008824e44f8bec22483fa37b25cde4ce5992fb
Yu Xiao <shawonline@gmail.com>
2023-11-09 14:02:21 +0800

    adjust structure of financial economics

--- financial_economics/volume.py
+++ financial_economics/volume.py
@@ -1,8 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import csv
+import article
 import time
 import re
+
+# 使用函数获取结果
+bare_href = "https://www.sciencedirect.com"
+base_href = "https://www.sciencedirect.com/journal/journal-of-financial-economics/issues"
+
+def process_all_journals():
+    volume_results = get_volume_issue(base_href)
+    # 创建一个 csv 文件并写入数据
+    with open('financial_economics.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # 写入表头
+        writer.writerow(['year', 'volume', 'issue', 'date', 'title', 'author', 'page/article no', 'doi', 'link', 'abstract'])
+        
+        # 遍历 volume_results 获取每个 volume 和 issue 的信息
+        for volume_issue in volume_results:
+            year = volume_issue[0]
+            volume_num = volume_issue[1]
+            issue = volume_issue[2]
+            date = volume_issue[3]
+            issue_url = bare_href + volume_issue[5]  # 假设是完整的 URL，如果不是请加上基础 URL
+            
+            # 使用 get_articles 函数获取文章列表
+            articles = article.get_articles(issue_url)
+            for item in articles:
+                writer.writerow([
+                    year, volume_num, issue, date,
+                    item['Title'], item['Author'], item['Page/Article No'], item['DOI'], item['Link'], item['Abstract']
+                ])
+
 
 def get_volume_issue(base_href):
     driver = webdriver.Chrome()
--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-from financial_economics import volume
-from financial_economics import article
-import csv
-import review_of_financial_studies.volume as volume
-
+from financial_economics import volume as fe
+import review_of_financial_studies.volume as rfs
+import journal_of_finance.volume as jf
 
 # 生成 review_of_financial_studies.csv 文件
-volume.process_all_journals(1, 36)
+rfs.process_all_journals(1, 36)
 
+# 生成 journal_of_finance.csv 文件
+jf.process_all_journals(1, 76)
 
-
-# 使用函数获取结果
-bare_href = "https://www.sciencedirect.com"
-base_href = "https://www.sciencedirect.com/journal/journal-of-financial-economics/issues"
-volume_results = volume.get_volume_issue(base_href)
-
-# 创建一个 csv 文件并写入数据
-with open('articles.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    # 写入表头
-    writer.writerow(['year', 'volume', 'issue', 'date', 'title', 'author', 'page/article no', 'doi', 'link', 'abstract'])
-    
-    # 遍历 volume_results 获取每个 volume 和 issue 的信息
-    for volume_issue in volume_results:
-        year = volume_issue[0]
-        volume_num = volume_issue[1]
-        issue = volume_issue[2]
-        date = volume_issue[3]
-        issue_url = bare_href + volume_issue[5]  # 假设是完整的 URL，如果不是请加上基础 URL
-        
-        # 使用 get_articles 函数获取文章列表
-        articles = article.get_articles(issue_url)
-        for item in articles:
-            writer.writerow([
-                year, volume_num, issue, date,
-                item['Title'], item['Author'], item['Page/Article No'], item['DOI'], item['Link'], item['Abstract']
-            ])
+# 生成 financial economics 文件
+fe.process_all_journals()
